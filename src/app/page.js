f5123a529f38a0e2dce90a78c6fdef0b7e93dff2
ebertm777 <johndoe@example.comeber.tmacedo@gmail.com>
2025-05-08 14:33:44 -0300

    minor changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,21 @@
 import TimelineChart from "@/Data/Data";
 
 const kingdoms = [
-  { name: "Reino de Eldoria", start: 0, end: 300 },
-  { name: "Império Draknor", start: 0, end: 450 },
-  { name: "Confederação Thalor", start: 0, end: 700 },
-  { name: "Domínio de Varkun", start: 0, end: 250 },
-  { name: "Terras de Myrvalen", start: 0, end: 500 },
-  { name: "Clãs de Norheim", start: 0, end: 350 },
-  { name: "Aliança de Zandor", start: 0, end: 600 },
-  { name: "Província de Quelmar", start: 0, end: 550 },
+  { name: "Player1", start: 0, end: 250 },
+  { name: "Player2", start: 0, end: 400 },
+  { name: "Player3", start: 0, end: 550 },
+  { name: "Player4", start: 0, end: 350 },
+  { name: "Player5", start: 0, end: 700 },
+  { name: "Player6", start: 0, end: 650 },
+  { name: "Player7", start: 0, end: 500 },
+  { name: "Player8", start: 0, end: 450 },
 ];
 
 export default function Home() {
   return (
     <div className="container">
-    <h1>📜 Linhas do Tempo dos Reinos</h1>
-    <TimelineChart data={kingdoms} />
-  </div>
+      <h1>📜 Contribuição dos aventureiros</h1>
+      <TimelineChart data={kingdoms} />
+    </div>
   );
 }
